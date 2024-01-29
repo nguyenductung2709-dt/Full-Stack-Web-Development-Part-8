95b134d8d46f90c1fed422d174cbb457dbfa0dcb
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-29 21:56:11 +0200

    8.5

--- library/index.js
+++ library/index.js
@@ -112,7 +112,7 @@ const typeDefs = `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String!): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -122,10 +122,17 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-        if (!args.author) {
-            return null;
-        }
+      if (!args.author && !args.genre) {
+        return books;
+      }
+    
+      if (args.author) {
         return books.filter(book => book.author === args.author);
+      }
+    
+      if (args.genre) {
+        return books.filter(book => book.genres.includes(args.genre));
+      }
     },
     allAuthors: () => authors,
   },
