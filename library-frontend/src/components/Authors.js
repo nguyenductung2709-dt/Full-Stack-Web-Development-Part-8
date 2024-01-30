9200b92173cfc938c9dc241024fd55b44d9201a9
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-30 13:51:57 +0200

    8.12

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -31,7 +31,7 @@ const Authors = (props) => {
         </tbody>
       </table>
     </div>
-    <BirthYear/>
+    <BirthYear authors = {authors}/>
     </>
   )
 }
--- library-frontend/src/components/BirthYear.js
+++ library-frontend/src/components/BirthYear.js
@@ -1,37 +1,43 @@
 import { useState } from 'react'
 import { useMutation } from '@apollo/client'
 import { EDIT_AUTHOR } from '../queries'
+import Select from 'react-select';
 
-const BirthYear = () => {
-    const [name, setName] = useState('')
-    const [year, setYear] = useState('')
-    const [ editAuthor ] = useMutation(EDIT_AUTHOR) 
+const BirthYear = ({ authors }) => {
+    const authorOptions = authors.map((author) => ({ value: author.name, label: author.name }));
+
+    const [selectedAuthor, setSelectedAuthor] = useState(null);
+    const [year, setYear] = useState('');
+    const [editAuthor] = useMutation(EDIT_AUTHOR);
+
     const submit = (event) => {
-        event.preventDefault()
-        editAuthor({ variables: { name, setBornTo: Number(year) } })
-        setName('')
-        setYear('')
+        event.preventDefault();
+        if (selectedAuthor) {
+            editAuthor({ variables: { name: selectedAuthor.value, setBornTo: Number(year) } });
+            setSelectedAuthor(null);
+            setYear('');
+        }
     }
-    return(
+
+    return (
         <div>
-            <h2> Set birthyear </h2>
-            <form onSubmit = {submit}>
-            <div>
-                name <input
-                    value={name}
-                    onChange={({ target }) => setName(target.value)}
+            <h2>Set birthyear</h2>
+            <form onSubmit={submit}>
+                <Select
+                    value={selectedAuthor}
+                    onChange={setSelectedAuthor}
+                    options={authorOptions}
                 />
-            </div>
-            <div>
-                born <input
-                    value={year}
-                    onChange={({ target }) => setYear(target.value)}
-                />
-            </div>
-        <button type='submit'>change number</button>
+                <div>
+                    born <input
+                        value={year}
+                        onChange={({ target }) => setYear(target.value)}
+                    />
+                </div>
+                <button type='submit'>change number</button>
             </form>
         </div>
-    )
+    );
 }
 
-export default BirthYear
+export default BirthYear;
