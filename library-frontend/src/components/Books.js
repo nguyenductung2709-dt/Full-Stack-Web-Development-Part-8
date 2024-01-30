896aae302db1a9f629e45a64f5be56d7b0f11e27
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-01-30 11:22:49 +0200

    8.9

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,10 +1,13 @@
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../queries'
 const Books = (props) => {
+  const result = useQuery(ALL_BOOKS)
+
   if (!props.show) {
     return null
   }
 
-  const books = []
-
+  const books = result.data ? result.data.allBooks : []
   return (
     <div>
       <h2>books</h2>
