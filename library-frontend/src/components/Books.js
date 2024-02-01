9e933cfe53a85cbca875c5c36a04d4078850eab6
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-02-02 01:22:21 +0200

    extract genres from books

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,13 +1,19 @@
 import { useQuery } from '@apollo/client'
 import { ALL_BOOKS } from '../queries'
+import { useState } from 'react'
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+
+  const [genre, setGenre] = useState('')
 
   if (!props.show) {
     return null
   }
 
   const books = result.data ? result.data.allBooks : []
+  const genresDefault = books.map(book => book.genres)
+  const genres = Array.from(new Set([].concat(...genresDefault)))
+  console.log(genres)
   return (
     <div>
       <h2>books</h2>
