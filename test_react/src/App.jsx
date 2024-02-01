828a8765d4a91be7f0a1157f04eba2c788090d26
Duc Tung Nguyen <tungdtnguyen2709@gmail.com>
2024-02-01 21:51:47 +0200

    test login

--- test_react/src/App.jsx
+++ test_react/src/App.jsx
@@ -1,18 +1,28 @@
-import { useQuery } from '@apollo/client'
+import { useQuery, useApolloClient } from '@apollo/client'
 import { useState } from 'react'
 
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Notify from './components/Notify'
 import PhoneForm from './components/PhoneForm'
+import LoginForm from './components/LoginForm'
 import { ALL_PERSONS } from './queries'
 
 const App = () => {
   const result = useQuery(ALL_PERSONS)
+  const [token, setToken] = useState(null)
   const [errorMessage, setErrorMessage] = useState(null)
+  const client = useApolloClient()
+
 
   if (result.loading)  {
     return <div>loading...</div>
+  }
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
   }
 
   const notify = (message) => {
@@ -20,6 +30,20 @@ const App = () => {
     setTimeout(() => {
       setErrorMessage(null)
     }, 10000)
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <button onClick={logout}>logout</button>
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={notify}
+        />
+      </div>
+    )
   }
 
   return (
